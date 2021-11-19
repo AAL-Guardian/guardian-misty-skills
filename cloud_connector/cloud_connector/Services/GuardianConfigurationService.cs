b579e3cb74150179ccf3dbfa34c57d186cb1182a
Risoft <rzwart@risoft.nl>
2021-11-19 12:02:32 +0100

    Made the configuration persistent between startups

--- cloud_connector/cloud_connector/Services/GuardianConfigurationService.cs
+++ cloud_connector/cloud_connector/Services/GuardianConfigurationService.cs
@@ -17,6 +17,7 @@ namespace CloudConnector.Services
         private readonly string _apiUrl;
         private readonly string _robotCode;
         private bool _resetConfig;
+        private readonly StorageFolder _storageFolder = ApplicationData.Current.LocalFolder;
 
         private MistyConfiguration _configuration;
 
@@ -40,29 +41,42 @@ namespace CloudConnector.Services
                 return _configuration;
             }
 
-            await _misty.SendDebugMessageAsync($"Getting config from: {_apiUrl}.");
-            var postData = "{\"robotCode\": \"" + _robotCode + "\"}";
-            var result =
-                await _misty.SendExternalRequestAsync("POST", _apiUrl, null, null, postData, false, false, null, null);
-            if (result.Status != ResponseStatus.Success || result.Data?.Data == null)
+            StorageFile configFile = (await _storageFolder.TryGetItemAsync("config.json")) as StorageFile;
+            if (configFile != null) // if config exists on misty then get that, else get from cloud
             {
-                await _misty.SendDebugMessageAsync("Couldn't get configuration from specified endpoint.");
-                // TODO throw error and try again in a couple of minutes
+                await _misty.SendDebugMessageAsync("Found existing config file, use that.");
+                string config = await FileIO.ReadTextAsync(configFile);
+                _configuration = JsonConvert.DeserializeObject<MistyConfiguration>(config);   
             }
+            else
+            {
+                await _misty.SendDebugMessageAsync($"No config file found locally, getting config from: {_apiUrl}.");
+                var postData = "{\"robotCode\": \"" + _robotCode + "\"}";
+                var result =
+                    await _misty.SendExternalRequestAsync("POST", _apiUrl, null, null, postData, false, false, null, null);
+                if (result.Status != ResponseStatus.Success || result.Data?.Data == null)
+                {
+                    await _misty.SendDebugMessageAsync("Couldn't get configuration from specified endpoint.");
+                    // TODO throw error and try again in a couple of minutes
+                }
+                
+                await _misty.SendDebugMessageAsync($"Saving config file locally...");
+                configFile = await _storageFolder.CreateFileAsync("config.json",CreationCollisionOption.ReplaceExisting);
+                await FileIO.WriteTextAsync(configFile, result.Data.Data as string);
 
-            _configuration = JsonConvert.DeserializeObject<MistyConfiguration>(result.Data.Data as string);
+                _configuration = JsonConvert.DeserializeObject<MistyConfiguration>(result.Data.Data as string);   
+            }
 
             await HandleCertificateFile();
             
-            await _misty.SendDebugMessageAsync("Received new config.");
+            await _misty.SendDebugMessageAsync("Got config.");
 
             return _configuration;
         }
 
         private async Task HandleCertificateFile()
         {
-            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
-            StorageFile pfxFile = await storageFolder.CreateFileAsync("cert.pfx",CreationCollisionOption.ReplaceExisting);
+            StorageFile pfxFile = await _storageFolder.CreateFileAsync("cert.pfx",CreationCollisionOption.ReplaceExisting);
 
             await FileIO.WriteBytesAsync(pfxFile, Convert.FromBase64String(_configuration.Certificate.PfxBase64));
 
